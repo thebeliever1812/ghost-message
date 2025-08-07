19f1fe173a6af445c9c690b85872a8a40a959e46
Basir Ahmad <basirahmadmalik@gmail.com>
2025-08-07 15:50:06 +0530

    added AI feature where it suggests 3 questions to user which can be sent to other user and after the backend part is almost completed

---
+++ src/app/api/suggest-messages/route.ts
@@ -0,0 +1,43 @@
+import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
+
+export async function GET() {
+	const model = google("gemini-2.5-flash");
+	const prompt =
+		"Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started? || If you could have dinner with any historical figure, who would it be?|| What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
+
+	try {
+		const { text } = await generateText({
+			model,
+			prompt,
+			providerOptions: {
+				google: {
+					safetySettings: [
+						{
+							category: "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+							threshold: "BLOCK_LOW_AND_ABOVE",
+						},
+					],
+					responseModalities: ["TEXT"],
+				},
+			},
+		});
+
+		return Response.json(
+			{
+				success: true,
+				text,
+			},
+			{ status: 200 }
+		);
+	} catch (error: any) {
+		console.error("AI generation error:", error);
+		return Response.json(
+			{
+				success: false,
+				message: "Failed to generate suggestions",
+			},
+			{ status: error.statusCode }
+		);
+	}
+}
